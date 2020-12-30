5ebb7c4a5e59da13f8bdf2180ab318d7d59d50c3
arkarmannaung <arkarmannaung@gmail.com>
2020-12-30 19:37:02 +0630

    communication

--- communication.blade.php
+++ communication.blade.php
@@ -1,12 +1,2 @@
-<link href="{{asset('vendors/fontawesome/css/font-awesome.css')}}" rel="stylesheet">
-
-
- <link href="{{asset('vendors/bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css')}}" rel="stylesheet">
-    <!-- Ion.RangeSlider -->
-    <link href="{{asset('vendors/normalize-css/normalize.css')}}" rel="stylesheet">
-    <link href="{{asset('vendors/ion.rangeSlider/css/ion.rangeSlider.css')}}" rel="stylesheet">
-    <link href="{{asset('vendors/ion.rangeSlider/css/ion.rangeSlider.skinFlat.css')}}" rel="stylesheet">
-    <!-- Bootstrap Colorpicker -->
-    <link href="{{asset('vendors/mjolnic-bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css')}}" rel="stylesheet">
-
-    <link href="{{asset('vendors/cropper/dist/cropper.min.css')}}" rel="stylesheet">
+<i class="fa fa-cog" aria-hidden="true"></i>
+<i class="fa fa-trash" aria-hidden="true"></i>
