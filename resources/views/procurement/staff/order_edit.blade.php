92f1f1bb426fb1af11735fd808e030f21153e9a0
arkarmannaung <arkarmannaung@gmail.com>
2021-01-04 08:21:18 +0630

    add finish button

--- resources/views/procurement/staff/order_edit.blade.php
+++ resources/views/procurement/staff/order_edit.blade.php
@@ -15,23 +15,25 @@ use App\Warehouse;
 
 	    <div class="clearfix"></div>
 		{{-- <div class="col-md-12 col-sm-12 "> --}}
-	        <div class="x_panel">
+{{-- 				<div class="x_title">
+				         <div class="page-title">
+					<div class="col-md-12 col-sm-12 ">
+				        <div class="x_panel">
+				        	<h4 class="text text-gray-dark d-inline-block pt-2">Go Finish! When supplier and price comfirm.</h4>
+			  				<button class="btn btn-danger pull-right pt-1">Finish</a>
+				        </div>	    
+				   	</div>
+			    </div>
+ --}}
 	          	<div class="x_title">
-	            	<h2>Order Request <small>please support price data from suitable supplier</small></h2>
+	            	<h2>Go Finish! <small>When supplier and price comfirm.</small></h2>
 	            	<ul class="nav navbar-right panel_toolbox">
-		              	<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
-		              	<li class="dropdown">
-		                	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-		                	<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-		                    	<a class="dropdown-item" href="">Add New</a> {{-- {{route('inventory.create')}} --}}
-		                    	<a class="dropdown-item" href="#">Settings 2</a>
-		                  	</div>
-		              	</li>
-		              	<li><a class="close-link"><i class="fa fa-close"></i></a>
-		              	</li>
+						<button class="btn btn-info pull-right pt-1">Back</a>
+						<button class="btn btn-danger pull-right pt-1">Finish</a>
 	            	</ul>
 	            	<div class="clearfix"></div>
 	          	</div>
+
 	          	<div class="x_content">
 	              	<div class="row">
 	                  	<div class="col-sm-12">
@@ -117,13 +119,15 @@ use App\Warehouse;
 	                  		</div>
 
 	                	</div>
-	                	<a class="btn btn-success pull-right">Finish</a>
+	                	
 	              	</div> 
 	            </div>
+	            
 	        </div>
 	    {{-- </div> --}}
 
   	</div>
+
 </div>
 <!-- /page content -->
 @endsection
